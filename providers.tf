8e68acbdeea7f48c1f195e189ddaa5052ad7c141
salman123-cyber <52010988+salman123-cyber@users.noreply.github.com>
2024-11-14 22:37:10 +0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,7 +11,7 @@ provider "azurerm" {
   client_id  = var.azure_client_id
   client_secret   =   var.azure_client_secret
   tenant_id       =   var.azure_tenent_id
-  subscription_id = var.azure_subscription_id
+  subscription_id = "29e23d23-aa38-4ea4-8036-d50a5b2c964c"
 }
 provider "kubernetes" {   
     host                   =    azurerm_kubernetes_cluster.example.kube_config.0.host
