c3a85d3f1bc55422631ef53cb4c84ea335cbe9c4
salman123-cyber <52010988+salman123-cyber@users.noreply.github.com>
2024-11-14 17:39:16 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,3 @@
-variable "p_az_clientID"{
-  type=string
-  default="def80782-17cc-44be-a66f-0449f651d052"
-}
-
-variable "p_az_secret"{
-  type=string
-  default="eAU8Q~u_Gb3i5XF~8QLPuIFjz7dyFCkwkZdf.c0o" 
-}
-
-variable "p_az_tenantID"{
-  type=string
-  default="b8284ad9-043c-44a3-8d63-5ce64a7a4970" 
-}
-
-variable "p_az_subscriptionID"{
-  type=string
-  default="29e23d23-aa38-4ea4-8036-d50a5b2c964c"
-}
-
 variable "n_count"{
   type=number
   default=2
