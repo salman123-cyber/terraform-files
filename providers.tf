04c23790095740154cc204483bc99ea527ab7b37
salman123-cyber <52010988+salman123-cyber@users.noreply.github.com>
2024-11-15 07:17:14 +0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.95.0"
+    }
+  }
+}
 provider "kubernetes" {   
     host                   =    azurerm_kubernetes_cluster.example.kube_config.0.host
     cluster_ca_certificate =    base64decode(azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate)
